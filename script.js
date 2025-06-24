13b3213bd02167bb2afadf130ebe44b01352c56b
Yana <sekretenkoana@gmail.com>
2025-06-24 12:19:00 +0300

    new

---
+++ script.js
@@ -0,0 +1,104 @@
+document.querySelectorAll('.faq-question').forEach(question => {
+  question.addEventListener('click', () => {
+    const item = question.parentElement;
+    item.classList.toggle('open');
+  });
+});
+
+function animateCounter(element) {
+  const target = +element.getAttribute('data-target');
+  const isPercent = element.textContent.includes('%');
+  let count = 0;
+  const step = target / 100;
+
+  const update = () => {
+    count += step;
+    if (count < target) {
+      element.textContent = Math.round(count) + (isPercent ? '%' : '');
+      requestAnimationFrame(update);
+    } else {
+      element.textContent = target + (isPercent ? '%' : '');
+    }
+  };
+  update();
+}
+
+const counters = document.querySelectorAll('.counter');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      animateCounter(entry.target);
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.6 });
+
+counters.forEach(counter => observer.observe(counter));
+
+const revealBlocks = document.querySelectorAll('.scroll-reveal');
+
+const server = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      server.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+revealBlocks.forEach(block => {
+  server.observe(block);
+});
+
+const scrollBtn = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+  const secondBlock = document.querySelector('.familiar-screen');
+  const triggerHeight = secondBlock?.offsetTop || 400;
+
+  if (window.scrollY > triggerHeight) {
+    scrollBtn.classList.add('visible');
+  } else {
+    scrollBtn.classList.remove('visible');
+  }
+});
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  scrollLock: true,
+  responsive: [
+    {
+     
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+     
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+setInterval(() => {
+  const glider = document.querySelector('.glider').glider;
+  glider.scrollItem((glider.slide + 1) % glider.slides.length);
+}, 4000);
+
